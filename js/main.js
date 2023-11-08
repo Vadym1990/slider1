06e6f1cb4a6acfeea3b8ea628ec297526951069b
Vadym Boiko <vadym.boiko1990@gmail.com>
2023-11-08 22:03:04 +0200

    omg

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const myAccBtn = document.querySelector('.my-acc-btn');
+const login = document.querySelector('.login');
+
+myAccBtn.onclick = function () {
+    login.classList.toggle('login_active')
+}
+
+// -------------------------- Slider --------------------------//
+
+const sliderImg = document.querySelectorAll('.slider-img');
+const btnNext = document.querySelector('.slider-btn_next');
+const btnPrev = document.querySelector('.slider-btn_prev');
+
+let currentSlide = 0;
+
+btnNext.onclick = function () {
+
+}
+
+
+
+
